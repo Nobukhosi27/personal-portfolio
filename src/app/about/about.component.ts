94fc770ab802a0c4580ad88918006cc346ad8c3a
Nobukhosi27 <nobukhosinxumalo27@gmail.com>
2024-11-11 12:30:32 +0200

    Added PersonalPortfolioWebsite files to repository

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,15 @@
+import { Component, inject } from '@angular/core';
+
+import { RouterLink } from '@angular/router';
+
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css'
+})
+export class AboutComponent {
+ 
+}
